e823832aa9d4d9cd47d96e984bc2d5ced9789e25
adityavamsi <chvamsiaditya33@gmail.com>
2024-03-20 18:54:52 +0530

    Completed simple chat app

---
+++ MiniProjects/chat-app/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const {Server} = require('socket.io');
+const app = express();
+const PORT = 5000;
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+//Socket.io
+io.on('connection',(socket)=>{
+    socket.on('user-message', (message)=>{
+        io.emit('message',message);
+    })
+});
+
+app.use(express.static(path.resolve('./public')));
+
+
+app.get('/',(req,res)=>{
+    return res.sendFile('/public/index.html');
+})
+
+server.listen(PORT,()=> console.log(`Server started at port no:${PORT}`));
